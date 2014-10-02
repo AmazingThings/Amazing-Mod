31357206e93e5b27729d97b2d68d4f3551c43f17
mathmods <mathias.beverfjord@gmail.com>
2014-10-02 17:33:45 +0200

    Working on it

---
+++ src/main/java/com/github/AmazingThings/Amazing_Mod/proxy/AmazingClientProxy.java
@@ -0,0 +1,10 @@
+package com.github.AmazingThings.Amazing_Mod.proxy;
+
+public class AmazingClientProxy extends AmazingCommonProxy {
+
+	@Override
+	public void registerRenderers(){
+		
+	}
+	
+}
---
+++ src/main/java/com/github/AmazingThings/Amazing_Mod/proxy/AmazingCommonProxy.java
@@ -0,0 +1,9 @@
+package com.github.AmazingThings.Amazing_Mod.proxy;
+
+public class AmazingCommonProxy {
+
+	public void registerRenderers(){
+		
+	}
+	
+}
