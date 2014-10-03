657d687525f34c5ba08d58aa223137395528feac
mathmods <mathias.beverfjord@gmail.com>
2014-10-03 17:33:00 +0200

    Added electric furnaces
    Still need tweaks

--- src/main/java/com/github/AmazingThings/Amazing_Mod/AmazingMain.java
+++ src/main/java/com/github/AmazingThings/Amazing_Mod/AmazingMain.java
@@ -1,10 +1,13 @@
 package com.github.AmazingThings.Amazing_Mod;
 
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 
+import com.github.AmazingThings.Amazing_Mod.block.ElectricFurnace;
 import com.github.AmazingThings.Amazing_Mod.proxy.AmazingCommonProxy;
+import com.github.AmazingThings.Amazing_Mod.tileentity.TileEntityElectricFurnace;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -13,10 +16,13 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid="amazingmod",version="1.0.0",name="Amazing Mod")
 public class AmazingMain {
 
+	public static String modid = "amazingmod";
+	
 	@Instance(value="amazingmod")
 	public static AmazingMain instance;
 	
@@ -33,13 +39,23 @@ public class AmazingMain {
 		
 	};
 	
+	public static Block ElectricFurnace;
+	public static Block ElectricFurnaceOn;
+	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent e){
-		
+		ElectricFurnace = new ElectricFurnace(false).setBlockName("electricfurnace").setCreativeTab(tabAmazing);
+		ElectricFurnaceOn = new ElectricFurnace(true).setBlockName("electricfurnaceon").setCreativeTab(tabAmazing);
 	}
 	
 	@EventHandler
 	public void Init(FMLInitializationEvent e){
+		
+		GameRegistry.registerBlock(ElectricFurnace, modid + ":electricfurnace");
+		GameRegistry.registerBlock(ElectricFurnaceOn, modid + ":electricfurnaceon");
+		
+		GameRegistry.registerTileEntity(TileEntityElectricFurnace.class, modid + ":electricfurnace");
+		
 		proxy.registerRenderers();
 	}
 	
--- src/main/java/com/github/AmazingThings/Amazing_Mod/proxy/AmazingClientProxy.java
+++ src/main/java/com/github/AmazingThings/Amazing_Mod/proxy/AmazingClientProxy.java
@@ -1,9 +1,16 @@
 package com.github.AmazingThings.Amazing_Mod.proxy;
 
+import com.github.AmazingThings.Amazing_Mod.renderer.TileEntityRendererElectricFurnace;
+import com.github.AmazingThings.Amazing_Mod.tileentity.TileEntityElectricFurnace;
+
+import cpw.mods.fml.client.registry.ClientRegistry;
+
 public class AmazingClientProxy extends AmazingCommonProxy {
 
 	@Override
 	public void registerRenderers(){
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityElectricFurnace.class,
+				new TileEntityRendererElectricFurnace());
 		
 	}
 	
